ea88bd952689f3c44fd1843b9798b92622d16b00
stack72 <public@paulstack.co.uk>
2022-06-05 17:34:15 +0300

    [internal] Disabling linting for the provider sdk

--- provider/cmd/pulumi-resource-confluentcloud/main.go
+++ provider/cmd/pulumi-resource-confluentcloud/main.go
@@ -17,9 +17,9 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	confluentcloud "github.com/pulumi/pulumi-confluentcloud/provider"
 	"github.com/pulumi/pulumi-confluentcloud/provider/pkg/version"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 )
 
 func main() {
