40c039a91c8679cc4c1fccdbf02dee665066e865
Ramon Quitales <ramon@pulumi.com>
2023-08-24 10:34:11 -0700

    Add integration test for patch

---
+++ examples/kafka-topic-patch/ts/step1/index.ts
@@ -0,0 +1,22 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as confluent from "@pulumi/confluentcloud";
+
+const env = new confluent.Environment("ts-env");
+
+const kfCluster = new confluent.KafkaCluster("kfCluster", {
+  cloud: "AWS",
+  region: "us-west-2",
+  availability: "SINGLE_ZONE",
+  environment: {
+    id: env.id,
+  },
+});
+
+const kfTopic = new confluent.KafkaTopic("kfTopic", {
+  topicName: "test-topic",
+  kafkaCluster: {
+    id: kfCluster.id,
+  },
+  // Deprecated field which we are still supporting.
+  httpEndpoint: kfCluster.restEndpoint,
+});
